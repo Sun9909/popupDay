170b3b42f848b069e88604f9f3551019dadee133
jeon <jejjb06@gmail.com>
2024-07-05 15:48:37 +0900

    팝업컨트롤러

--- src/main/java/flower/popupday/PopupController.java
+++ src/main/java/flower/popupday/PopupController.java
@@ -9,11 +9,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PopupController {
 
-    @GetMapping("/join.do") // (매핑이름) ex : ("/popupList.do)
-    public ModelAndView join(HttpServletRequest request,
+    @GetMapping("/popupList.do") // (매핑이름) ex : ("/popupList.do)
+    public ModelAndView popupList(HttpServletRequest request,
                              HttpServletResponse response) throws Exception {
         ModelAndView mav=new ModelAndView();
-        mav.setViewName("/login/join"); // "/폴더이름/파일이름 " ex ) ("/mypage/popupList")
+        mav.setViewName("/mypage/popupList"); // "/폴더이름/파일이름 " ex ) ("/mypage/popupList")
         return mav;
     }
 }
