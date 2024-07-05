8288da2d61d02b128d7e4ddb6c12ce9806b15176
Sin <ekdms008@gmail.com>
2024-07-05 22:06:36 +0900

    popup

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -1,1 +1,18 @@
-    })
+$(document).ready(function(){
+    $("#search-banner .search").click(function() {
+        var submenu = $(this).next("div");
+
+        if(submenu.is(":visible")) {
+            submenu.slideUp();
+        }else{
+            submenu.slideDown();
+        }
+    });
+});
+
+const myCarouselElement = document.querySelector('#myCarousel')
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+
+})
