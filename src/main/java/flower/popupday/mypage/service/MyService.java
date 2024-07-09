30fb6c75229471e3eb82709aafc7727f0326e6ab
jeon <jejjb06@gmail.com>
2024-07-09 19:40:42 +0900

    백엔드 해보기

---
+++ src/main/java/flower/popupday/mypage/service/MyService.java
@@ -0,0 +1,13 @@
+package flower.popupday.mypage.service;
+
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public interface MyService {
+    public List membersList() throws DataAccessException;
+
+    public void findId(Long id) throws DataAccessException;
+
+    public void findName(String user_nikname) throw DataAccessException;
+}
