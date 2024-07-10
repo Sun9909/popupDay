03902ed762791c5692ce7bd8dd1214dad95f3299
Sin <ekdms008@gmail.com>
2024-07-10 18:13:44 +0900

    faq완성

--- src/main/java/flower/popupday/notice/faq/controller/FaqControllerImpl.java
+++ src/main/java/flower/popupday/notice/faq/controller/FaqControllerImpl.java
@@ -1,14 +1,10 @@
 package flower.popupday.notice.faq.controller;
 import flower.popupday.notice.faq.dto.FaqDTO;
 import flower.popupday.notice.faq.service.FaqService;
-import flower.popupday.notice.faq.service.FaqServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,7 +19,7 @@ public class FaqControllerImpl implements FaqController {
     private FaqDTO faqDTO;
 
     @Autowired
-    private FaqServiceImpl faqService;
+    private FaqService faqService;
 
 
     //Faq작성 폼으로 이동
