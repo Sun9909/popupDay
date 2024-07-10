5642b2c90fd00dee595c4065db91b3557b40adf3
jeon <jejjb06@gmail.com>
2024-07-10 18:46:41 +0900

    Merge pull request #84 from Sun9909/Flower
    마이페이지 백엔드

--- src/main/java/flower/popupday/mypage/controller/MyControllerImpl.java
+++ src/main/java/flower/popupday/mypage/controller/MyControllerImpl.java
@@ -6,7 +6,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,13 +23,14 @@ public class MyControllerImpl implements MyController {
     @Autowired
     private MyDTO myDTO;
 
-    @PostMapping("/mypage/memberPage.do")
-    public ModelAndView login(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        List membersList = myService.membersList();
+    @Override
+    @RequestMapping("/mypage/memberPage.do") //
+    public ModelAndView getName(@ModelAttribute("myDTO") MyDTO myDTO, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.setCharacterEncoding("utf-8");
+        myDTO=myService.getName(myDTO);
         ModelAndView mav = new ModelAndView();
-        mav.setViewName("/mypage/memberPage");
-        mav.addObject("membersList", membersList);
+        mav.setViewName("mypage/memberPage");
+        mav.addObject("my",myDTO);
         return mav;
-
     }
 }
