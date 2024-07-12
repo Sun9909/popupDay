b0fc5219b4180fa2a8a67b42fa61e18a08ad2fe0
서누 <tjsdn909@gmail.com>
2024-07-12 11:58:28 +0900

    Create ReviewService.java

---
+++ src/main/java/flower/popupday/notice/review/service/ReviewService.java
@@ -0,0 +1,18 @@
+package flower.popupday.notice.review.service;
+
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ReviewService {
+
+    int addReview(Map<String, Object> reviewMap);
+
+    // 글목록 로그인 했을때는 map으로 확인
+    //public Map listArticles(Map<String, Integer> pageingMap) throws DataAccessException;
+
+    // 로그인 안 했을때는 list로
+
+    public List reviewList() throws DataAccessException;
+}
