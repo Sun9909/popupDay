25ebeb3258c52bda347c8f2a1e48f2aa9b0fbf80
hsy <skyn116@naver.com>
2024-07-12 19:24:25 +0900

    tnwjd

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,107 +1,78 @@
-var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-    mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
+var script = document.createElement('script');
+script.src = 'http://t1.daumcdn.net/mapjsapi/js/main/4.4.19/kakao.js';
+document.head.appendChild(script);
+
+document.addEventListener('DOMContentLoaded', function() {
+    var mapContainer = document.getElementById('map');
+    var mapOption = {
+        center: new kakao.maps.LatLng(37.566826, 126.9786567),
+        level: 1
     };
-
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-var map = new kakao.maps.Map(mapContainer, mapOption);
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
 
-    var map;
-    var mapContainer;
-    var mapOption;
-    var ps;
+    // 지도를 생성합니다
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-    $(document).ready(function () {
-    // 마커 표시
-    reloadMarker();
-
-    //검색
-    $(".search_wrapper.active").on("click", "#submitBtn", function() {
-    searchPlaces();
-});
-})
-; // document ready end
-
-    // 상점 마커 표시 Ajax
-    function reloadMarker(){
-    var params = $("#actionForm").serialize();
-
-    $.ajax({
-    url : "MainAjax",
-    type : "POST",
-    datatype : "json",
-    data : params,
-    success : function(res){
-    drawMarker(res.mMarker);
-},
-    error : function(request, status, error){
-    console.log(request.responseText);
-}
-});
-}
-
-    // 지도 생성, 마커 표시
-    function drawMarker(mMarker) {
-    if(map == null) {
-    mapContainer = document.getElementById('map'); //지도 표시할 div
-    mapOption = {
-    center: new kakao.maps.LatLng(37.5666805, 126.9784147), //지도의 중심 좌표 (서울 시청)
-    level: 3 //지도의 확대 레벨
-};
-
-    //지도 생성
-    map = new kakao.maps.Map(mapContainer, mapOption);
-
-    // 장소 검색 객체
-    ps = new kakao.maps.services.Places();
-}
-
-    // 마커 이미지 주소
-    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
-    // 마커 이미지의 이미지 크기
-    var imageSize = new kakao.maps.Size(24, 35);
-    // 마커 이미지 생성
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
-
-    // 주소-좌표 변환 객체
+    // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder();
 
-    mMarker.forEach(function(data) {
-    // 주소로 좌표 검색
-    geocoder.addressSearch(data.MK_ADR, function(result, status) {
-    if (status === kakao.maps.services.Status.OK) {
-    var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+    var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
+        infowindow = new kakao.maps.InfoWindow({zindex: 1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
 
-    // 결과값으로 받은 위치를 마커로 표시합니다
-    var marker = new kakao.maps.Marker({
-    title: data.MK_NAME,
-    map: map,
-    position: coords,
-    image : markerImage // 마커 이미지
+    // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+    searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+
+    // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'click', function (mouseEvent) {
+        searchDetailAddrFromCoords(mouseEvent.latLng, function (result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+                var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+                detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+
+                var content = '<div class="bAddr">' +
+                    '<span class="title">법정동 주소정보</span>' +
+                    detailAddr +
+                    '</div>';
+
+                // 마커를 클릭한 위치에 표시합니다
+                marker.setPosition(mouseEvent.latLng);
+                marker.setMap(map);
+
+                // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
+                infowindow.setContent(content);
+                infowindow.open(map, marker);
+            }
+        });
+    });
+
+    // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
+    kakao.maps.event.addListener(map, 'idle', function () {
+        searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+    });
+
+    function searchAddrFromCoords(coords, callback) {
+        // 좌표로 행정동 주소 정보를 요청합니다
+        geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+    }
+
+    function searchDetailAddrFromCoords(coords, callback) {
+        // 좌표로 법정동 상세 주소 정보를 요청합니다
+        geocoder.coord2Address(coords.getLng(), coords.getLat(), callback);
+    }
+
+    // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+    function displayCenterInfo(result, status) {
+        if (status === kakao.maps.services.Status.OK) {
+            var infoDiv = document.getElementById('centerAddr');
+
+            for (var i = 0; i < result.length; i++) {
+                // 행정동의 region_type 값은 'H' 이므로
+                if (result[i].region_type === 'H') {
+                    infoDiv.innerHTML = result[i].address_name;
+                    break;
+                }
+            }
+        }
+    }
 });
-
-    // 인포윈도우 닫기 버튼
-    var iwRemoveable = true;
-
-    // 인포윈도우로 장소에 대한 설명 표시
-    var infowindow = new kakao.maps.InfoWindow({
-    content : '<div id="goInfo">'+ data.MK_NAME +'</div>',
-    removable : iwRemoveable,
-    zIndex : 1
-});
-
-    // 마커에 클릭이벤트를 등록
-    kakao.maps.event.addListener(marker, 'click', function() {
-    // 다른 열린 인포윈도우는 닫게 함
-    $("img[alt='close']").click();
-    // 마커 위에 인포윈도우 표시
-    infowindow.open(map, marker);
-});
-}
-})
-});
-}
-
-
