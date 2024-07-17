eb7c3c8de99eab45116aa1bdc8d3f5937adde7d9
Sin <ekdms008@gmail.com>
2024-07-18 01:36:46 +0900

    ..

--- src/main/java/flower/popupday/common/FileDownloadController.java
+++ src/main/java/flower/popupday/common/FileDownloadController.java
@@ -15,19 +15,19 @@ public class FileDownloadController extends HttpServlet {
     private static String ARTICLE_IMG_REPO="D:\\Sin\\fileupload2";
 
     @RequestMapping("/download.do")
-    public void fileDown(@RequestParam("review_id") String review_id, @RequestParam("image_file_name") String image_file_name,
+    public void fileDown(@RequestParam("articleNo") String articleNo, @RequestParam("imageFileName") String imageFileName,
                          HttpServletRequest request, HttpServletResponse response) throws Exception {
 
         request.setCharacterEncoding("utf-8");
         response.setContentType("text/html;charset=utf-8");
         // 글 번호와 이미지 파일이름 가져오기.
         OutputStream outs=response.getOutputStream(); // input 읽어오기 output 읽어온걸 화면에 보여줌
-        String path=ARTICLE_IMG_REPO + "\\" + review_id + "\\" + image_file_name; // 이미지 경로, 이름
+        String path=ARTICLE_IMG_REPO + "\\" + articleNo + "\\" + imageFileName; // 이미지 경로, 이름
         File imageFile=new File(path);
         response.setHeader("Cache-Control","no-cache");
 
         // 이미지 파일 내려받는데(다운로드) 필요한 response 헤더 정보 설정
-        response.addHeader("Content-disposition", "attachment;fileName=" + image_file_name); // attachement : 추가 fileName : 변수
+        response.addHeader("Content-disposition", "attachment;fileName=" + imageFileName); // attachement : 추가 fileName : 변수
         FileInputStream fis=new FileInputStream(imageFile); // imageFile 에는 경로가 있음
         byte[] buffer=new byte[1024*8]; // 버퍼를 이용해 한번에 8kbyte씩 화면에 전송
         while(true) {
