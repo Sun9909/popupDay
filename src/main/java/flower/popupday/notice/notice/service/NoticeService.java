00f1e0e61edceaf6db75e0e23d9a41a28899f33b
yoni <wldus123164@gmail.com>
2024-07-18 16:43:29 +0900

    수정완료

--- src/main/java/flower/popupday/notice/notice/service/NoticeService.java
+++ src/main/java/flower/popupday/notice/notice/service/NoticeService.java
@@ -1,25 +1,24 @@
 package flower.popupday.notice.notice.service;
 
 import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
 
 import java.util.Map;
-
 public interface NoticeService {
 
-    // 글목록 페이징처리
+    //글 목록, 페이징처리
     public Map noticeList(Map<String, Integer> pagingMap) throws DataAccessException;
 
-    // 여러개의 이미지 추가
-    public int addNotice(Map<String, Object> articleMap) throws DataAccessException;
+    // 글쓰기 + 이미지 여러개 추가
+    public int addNotice(Map<String, Object> noticeMap) throws DataAccessException;
 
-    // 상세글 보기 (글번호를 가지고 상세 글 보기)
-    public Map adminNoticeView(Long notice_id) throws DataAccessException;
+    // 여러개의 글과 이미지 상세 글보기
+    public Map noticeView(Long notice_id) throws DataAccessException;
 
-    // 여러개의 이미지 글 수정
-    public void modNotice(Map<String, Object> articleMap) throws DataAccessException;
+    // 여러개의 글과 이미지 수정
+    public void modNotice(Map noticeMap) throws DataAccessException;
 
-    // 글삭제
+    // 글,이미지 삭제
     public void removeNotice(Long notice_id) throws DataAccessException;
 
 }
-
