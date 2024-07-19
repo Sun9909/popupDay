81f5534897944fbde7b2f5cbfffa3c87546ce5aa
yoni <wldus123164@gmail.com>
2024-07-19 10:04:44 +0900

    한다커밋

---
+++ src/main/java/flower/popupday/admin/dao/AdminDAO.java
@@ -0,0 +1,21 @@
+package flower.popupday.admin.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+@Mapper
+public interface AdminDAO {
+    public List selectAllMembersList() throws DataAccessException;
+
+    public AdminDTO selectMemberById(Long id) throws DataAccessException;
+
+    public void updateMember(AdminDTO adminDTO) throws DataAccessException;
+
+    public void delMember(Long id) throws DataAccessException;
+
+    boolean checkId(String user_id) throws DataAccessException;
+
+    boolean checkNikname(String user_nikname) throws DataAccessException;
+}
