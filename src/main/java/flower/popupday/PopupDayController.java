6fe9bd31b35a67bbcd212d91546030e97308494a
Sin <ekdms008@gmail.com>
2024-07-22 23:08:51 +0900

    html,css수정

--- src/main/java/flower/popupday/PopupDayController.java
+++ src/main/java/flower/popupday/PopupDayController.java
@@ -20,4 +20,14 @@ public class PopupDayController {
         // ModelAndView 객체를 반환
         return mav;
     }
+
+    @GetMapping("/main2.do")
+    public ModelAndView main2(HttpServletRequest request,
+                             HttpServletResponse response) throws Exception {
+        ModelAndView mav = new ModelAndView();
+        // main 뷰를 설정 (main.html 또는 main.jsp 파일이 있어야 함)
+        mav.setViewName("/popup/popupForm");
+        // ModelAndView 객체를 반환
+        return mav;
+    }
 }
