641cb0150692729f6abc99ec328e2bcdd30c2930
Sin <ekdms008@gmail.com>
2024-07-28 18:48:26 +0900

    프론트

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -3,4 +3,14 @@ $("#frames").on("load", function() {
     head.append("<link rel='stylesheet' href='/js/admin.css' type='text/css'>");
 });
 
+function adjustWidth2(input) {
+    // 글자 수에 따른 너비를 ch 단위로 설정
+    const length = input.value.length;
+    input.style.width = (length * 15 + 15) + 'px';
+}
 
+// 페이지 로드 시, 모든 input 요소의 너비를 초기화
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('#tag');
+    inputs.forEach(input => adjustWidth2(input));
+});
